45145231a4117687457a16f3afabb1739c57eefb
Anupam <anupamsinghj@gmail.com>
2020-09-09 20:14:16 +0530

    superuser and database is created

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class COORD(models.Model):
+
+    lat = models.FloatField()
+    img = models.ImageField(upload_to='pics')
+    lon = models.FloatField()
