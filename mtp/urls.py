a16c4235da3c696b728f7d8a31d33c71b9b435c6
Anupam <anupamsinghj@gmail.com>
2020-09-09 23:57:10 +0530

    superuser and database is created

--- mtp/urls.py
+++ mtp/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('hello.urls')),
     path('admin/', admin.site.urls),
-] 
+] + static(settings.MEDIA_URL,
+document_root=settings.MEDIA_ROOT)
