ea52dd7f5565c890cffd8567474b577f9f71b50c
Anupam <anupamsinghj@gmail.com>
2020-09-11 00:06:09 +0530

    import_export data

--- hello/admin.py
+++ hello/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import COORD
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
-admin.site.register(COORD)
+#admin.site.register(COORD)
+
+@admin.register(COORD)
+class usrdet(ImportExportModelAdmin):
+    pass
